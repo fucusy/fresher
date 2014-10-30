91047c97e1e3856e485e08efa8e2d019e9ff77c0
John <fucusy@gmail.com>
2014-10-30 20:49:14 +0800

    add php android client backend system

---
+++ server/php_android_backend/gcm_server_php/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "fresher");
+
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyBouov6heiDYz-U2G5irQ8C5onnoJ19Xqs"); // Place your Google API Key
+?>
